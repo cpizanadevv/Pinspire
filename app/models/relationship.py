f4344e3ee1b44e606a1c96c4d19fa420ba64d073
Penelope Yang <penelope.yang21@gmail.com>
2024-08-13 16:21:21 -0500

    render test 7

--- app/models/relationship.py
+++ app/models/relationship.py
@@ -1,12 +1,12 @@
-from .db import db, environment, SCHEMA
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 from sqlalchemy.schema import Column, ForeignKey, Table
 
 # Join table for boards and pins many-to-many relationship
 board_pins = db.Table(
     "board_pins",
     db.Model.metadata,
-    db.Column("board_id", db.ForeignKey("boards.id"), primary_key=True),
-    db.Column("pin_id", db.ForeignKey("pins.id"), primary_key=True)
+    Column("board_id", ForeignKey(add_prefix_for_prod("boards.id")), primary_key=True),
+    Column("pin_id", ForeignKey(add_prefix_for_prod("pins.id")), primary_key=True)
 )
 
 if environment == "production":
@@ -16,8 +16,8 @@ if environment == "production":
 pin_tags = db.Table(
     "pin_tags",
     db.Model.metadata,
-    db.Column("pin_id", db.ForeignKey("pins.id"), primary_key=True),
-    db.Column("tag_id", db.ForeignKey("tags.id"), primary_key=True)
+    Column("pin_id", ForeignKey(add_prefix_for_prod("pins.id")), primary_key=True),
+    Column("tag_id", ForeignKey(add_prefix_for_prod("tags.id")), primary_key=True)
 )
 
 if environment == "production":
