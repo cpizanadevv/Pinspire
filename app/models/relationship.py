0d27e432ab21c442be1aec33b35f97bc378ceb9a
Penelope Yang <penelope.yang21@gmail.com>
2024-08-13 15:16:14 -0500

    render test 3

--- app/models/relationship.py
+++ app/models/relationship.py
@@ -4,9 +4,9 @@ from sqlalchemy.schema import Column, ForeignKey, Table
 # Join table for boards and pins many-to-many relationship
 board_pins = db.Table(
     "board_pins",
-    db.metadata,
-    Column("board_id", ForeignKey("boards.id"), primary_key=True),
-    Column("pin_id", ForeignKey("pins.id"), primary_key=True)
+    db.Model.metadata,
+    db.Column("board_id", ForeignKey("boards.id"), primary_key=True),
+    db.Column("pin_id", ForeignKey("pins.id"), primary_key=True)
 )
 
 if environment == "production":
@@ -15,9 +15,9 @@ if environment == "production":
 # Join table for tags and pins many-to-many relationship
 pin_tags = db.Table(
     "pin_tags",
-    db.metadata,
-    Column("pin_id", ForeignKey("pins.id"), primary_key=True),
-    Column("tag_id", ForeignKey("tags.id"), primary_key=True)
+    db.Model.metadata,
+    db.Column("pin_id", ForeignKey("pins.id"), primary_key=True),
+    db.Column("tag_id", ForeignKey("tags.id"), primary_key=True)
 )
 
 if environment == "production":
