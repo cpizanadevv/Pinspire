4d499283cc418bcc040413a17a5432d7b7d6e3ec
Nicole Magallanes <nbmagallanesflores@gmail.com>
2024-08-14 15:33:34 -0700

    Added user information to Profile component

--- react-vite/src/components/Profile/ProfilePage.jsx
+++ react-vite/src/components/Profile/ProfilePage.jsx
@@ -1,19 +1,58 @@
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useParams, useNavigate } from 'react-router-dom';
 import "./ProfilePage.css";
-import { NavLink } from "react-router-dom";
+
 
 const Profile = () => {
     const user = useSelector((state) => state.session.user);
-    console.log("stuff", user);
+
+    const [activeTab, setActiveTab] = useState('saved');
+
+    const navigate = useNavigate()
+    const { userId } = useParams()
+
+    useEffect(() => {
+        if (!user || user.id !== +userId) {
+            navigate('/');
+        }
+    }, [user, userId, navigate]);
+
+    const handleTabChange = (tab) => {
+        setActiveTab(tab);
+    };
 
     return (
-        <div>
-            <div id="user-container">
-                <h1>
-                    {user.first_name} {user.last_name}
-                </h1>
-                <p>{user.username}</p>
+        <div id="profile-page-container">
+            <div id="profile-top-container">
+                <div id="profile-user-container">
+                    <span id="profile-picture">{user.last_name[0]}</span>
+                    <h1>{user.first_name} {user.last_name}</h1>
+                    <p>{user.username}</p>
+                    <div className="profile-buttons">
+                        <button>Share</button>
+                        <button> Edit profile</button>
+                    </div>
+                </div>
+                <div className="profile-links">
+                    <div className={activeTab === 'created' ? 'active' : ''}
+                    onClick={() => handleTabChange('created')}>
+                        Created
+                    </div>
+                    <div className={activeTab === 'saved' ? 'active' : ''}
+                    onClick={() => handleTabChange('saved')}>
+                        Saved
+                    </div>
+                    <div className={activeTab === 'favorites' ? 'active' : ''}
+                    onClick={() => handleTabChange('favorites')}>
+                        Favorites
+                    </div>
+                </div>
+            </div>
+            <div id="profile-bottom-container">
+                {activeTab === 'created' && <div>Created place holder.</div>}
+                {activeTab === 'saved' && <div>Saved place holder.</div>}
+                {activeTab === 'favorites' && <div>Favorites place holder</div>}
             </div>
         </div>
     );
--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -31,7 +31,7 @@ export const router = createBrowserRouter([
         element: <EditPin />,
       },
       {
-        path:"/profile",
+        path:":userId",
         element: <Profile />
       }
     ],
