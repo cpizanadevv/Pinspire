6b5c50a9155316decc7a3256c534442d6e1ba7f9
cj <c.pizana.devv@gmail.com>
2024-08-18 22:58:46 -0700

    testing something

--- react-vite/src/components/LandingPage/SearchPins.jsx
+++ react-vite/src/components/LandingPage/SearchPins.jsx
@@ -1,63 +1,113 @@
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import * as pinActions from '../../redux/pins'
+import * as pinActions from "../../redux/pins";
 import { NavLink, useParams } from "react-router-dom";
-import './LandingPage.css'
+import "./LandingPage.css";
+
 // import PinComponent from './PinComponent'
 
-// Function to shuffle pins on load to show variety
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+
+function SearchPins() {
+  const dispatch = useDispatch();
+  const [selectedBoardId, setSelectedBoardId] = useState(null);
+  const [savedPins, setSavedPins] = useState({});
+
+  const { keyword } = useParams();
+
+  const currentUserId = useSelector((state) => state.session.user?.id);
+  
+  const boards = useSelector((state) => state.boardState || []);
+  const boardsObj = Object.values(boards);
+  
+  useEffect(() => {
+    dispatch(boardActions.fetchAllBoards());
+  }, [dispatch]);
+
+  const userBoards = useMemo(
+    () => boardsObj.filter((board) => board.user_id === currentUserId),
+    [boardsObj, currentUserId]
+  );
+
+  const allPinsBoard = useMemo(
+    () =>
+      boardsObj.find(
+        (board) => board.name === "All Pins" && board.user_id === currentUserId
+      ),
+    [boardsObj, currentUserId]
+  );
+
+  const handleSave = (pinId) => {
+    if (selectedBoardId && allPinsBoard) {
+      dispatch(boardActions.postBoardPin(selectedBoardId, pinId));
+      setSavedPins((prevState) => ({
+        ...prevState,
+        [selectedBoardId]: new Set(prevState[selectedBoardId] || []).add(pinId),
+      }));
     }
-    return array;
-}
+  };
 
+  const isPinSaved = (pinId, boardId) => {
+    return savedPins[boardId]?.has(pinId);
+  };
 
-function SearchPins(){
-    const dispatch = useDispatch();
+  const pins = useSelector((state) => state.pinState.pins || {});
 
-    const {keyword} = useParams();
+  const allPins = Object.values(pins);
 
-    const pins = useSelector((state) => state.pinState.pins || {} );
-    // console.log('THIS IS PINS',pins)
-    
-    const allPins = Object.values(pins);
-    
-    const filtered = allPins.filter(pin => 
-        pin.title.toLowerCase().includes(keyword.toLowerCase())
-    );
-    // console.log('THIS IS filtered', filtered)
-    shuffleArray(filtered)
-    
-    useEffect(()=>{
-        dispatch(pinActions.getAllPins())
-    },[dispatch])
+  const filtered = allPins.filter((pin) =>
+    pin.title.toLowerCase().includes(keyword.toLowerCase())
+  );
 
+  useEffect(() => {
+    dispatch(pinActions.getAllPins());
+  }, [dispatch]);
 
-    return (
-        <div className="pins">
-            {filtered && filtered.map(({
-                id,
-                img_url,
-                // user_id,
-                // title,
-                // description,
-                // link
-            }) =>(
-                <div key={id} className="pin">
-                    <NavLink key={id} to={`/pin/${id}`}><img src={img_url} alt="" className="img"/></NavLink>
+  return (
+    <div className="created-grid-container">
+      {filtered && filtered.length > 0 ? (
+        <div className="search">
+          {filtered.map(({ id, img_url }) => (
+            <NavLink key={id} to={`/pin/${id}`}>
+              <div className="pin-container">
+                <img src={img_url} alt={`Pin ${id}`} />
+                <div className="image-overlay">
+                  <div className="board-container">
+                    <h4 className="save-to-board-text">Save to Board</h4>
+                    <select
+                      className="overlay-text"
+                      value={selectedBoardId || ""}
+                      onChange={(e) => setSelectedBoardId(e.target.value)}
+                    >
+                      <option value="" disabled>
+                        Select a Board
+                      </option>
+                      {userBoards.map((board) => (
+                        <option key={board.id} value={board.id}>
+                          {board.name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <button
+                    className={`landing-save-button ${
+                      isPinSaved(id, selectedBoardId) ? "saved" : ""
+                    }`}
+                    onClick={() => handleSave(id)}
+                  >
+                    {isPinSaved(id, selectedBoardId) ? "Saved" : "Save"}
+                  </button>
                 </div>
-                
-            ))}
-        
-            
+              </div>
+            </NavLink>
+          ))}
         </div>
-        
-
-    )
-    
+      ) : (
+        <div className="no-results">
+          <p>No pins with {keyword} in their title were found.</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default SearchPins;
